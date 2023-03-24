eb33ac5faad9e2fb02931c694bf2a3cb869301dc
Anugrah K <anugrahkorembeth.ak@gmail.com>
2023-03-24 17:27:28 +0530

    fixes

--- lib/views/wifi_info_view.dart
+++ lib/views/wifi_info_view.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:network_info_plus/network_info_plus.dart';
-import 'package:wifi_iot/wifi_iot.dart';
 import 'package:wifi_scan/wifi_scan.dart';
 
 class WifiInfoViewScreen extends StatelessWidget {
@@ -16,40 +12,80 @@ class WifiInfoViewScreen extends StatelessWidget {
       ),
       backgroundColor: const Color.fromARGB(255, 247, 247, 247),
       body: SafeArea(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          FutureBuilder(
-              future: WiFiScan.instance.getScannedResults(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    padding: const EdgeInsets.all(8),
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (context, index) => ListTile(
-                      tileColor: Colors.white,
-                      style: ListTileStyle.list,
-                      leading: CircleAvatar(
-                        
-                          backgroundColor:
-                              const Color.fromARGB(255, 247, 247, 247),
-                          child: Text((index + 1).toString(),),),
-                      title: Text(snapshot.data![index].ssid),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(snapshot.data![index].bssid),
-                        ],
-                      ),
-                    ),
-                  );
-                }
-                return const CircularProgressIndicator();
-              }),
-        ],
-      )),
+          child: FutureBuilder(
+                      future: WiFiScan.instance.startScan(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done &&
+                            snapshot.hasData) {
+                          return FutureBuilder(
+                              future: WiFiScan.instance.canGetScannedResults(),
+                              builder: (context, snapshot) {
+                                if (snapshot.connectionState ==
+                                        ConnectionState.done &&
+                                    snapshot.hasData &&
+                                    snapshot.data == CanGetScannedResults.yes) {
+                                  return FutureBuilder(
+                                      future:
+                                          WiFiScan.instance.getScannedResults(),
+                                      builder: (context, snapshot) {
+
+                                        if(snapshot.connectionState==ConnectionState.done &&snapshot.hasData){
+                                          return ListView.builder(
+                                            shrinkWrap: true,
+                                            physics:
+                                                const NeverScrollableScrollPhysics(),
+                                            padding: const EdgeInsets.all(8),
+                                            itemCount: snapshot.data!.length,
+                                            itemBuilder: (context, index) =>
+                                                ListTile(
+                                              tileColor: Colors.white,
+                                              style: ListTileStyle.list,
+                                              leading: CircleAvatar(
+                                                backgroundColor:
+                                                    const Color.fromARGB(
+                                                        255, 247, 247, 247),
+                                                child: Text(
+                                                  (index + 1).toString(),
+                                                ),
+                                              ),
+                                              title: Text(
+                                                  snapshot.data![index].ssid),
+                                              subtitle: Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  Text(snapshot
+                                                      .data![index].bssid),
+                                                ],
+                                              ),
+                                            ),
+                                          );
+                                        }
+                                        else {
+                                          return const Center(
+                                              child:
+                                                  CircularProgressIndicator());
+                                        }
+                                      
+                                      });
+                                } else if(snapshot.hasData && snapshot.data!=CanGetScannedResults.yes) {
+                                  return Text(snapshot.data!.toString());
+                                }
+                                else{
+                                  return const Center(
+                                      child: CircularProgressIndicator()); 
+                                }
+                              });
+                        } else if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        } else {
+                          return const Center(
+                            child: Text('No data Available'),
+                          );
+                        }
+                      })),
     );
   }
 }
